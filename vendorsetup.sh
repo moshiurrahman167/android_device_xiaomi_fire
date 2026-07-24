9ffa8506fe2c7b2059b8838cd6b53ee717965fbc
moshiurrahman167 <141856473+moshiurrahman167@users.noreply.github.com>
2026-07-24 16:44:02 +0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,1 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo omni_fire-user
-add_lunch_combo omni_fire-userdebug
-add_lunch_combo omni_fire-eng
+# Vendorsetup for TWRP fire
