b2c0f642250fea3d3433d082a6041142f259c853
moshiurrahman167 <141856473+moshiurrahman167@users.noreply.github.com>
2026-07-24 13:25:46 +0600

    Add files via upload

---
+++ omni_fire.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from fire device
+$(call inherit-product, device/xiaomi/fire/device.mk)
+
+PRODUCT_DEVICE := fire
+PRODUCT_NAME := omni_fire
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := fire
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_fire-user 15 AP3A.240905.015.A2 OS2.0.204.0.VMXINXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/vnd_fire/fire:15/AP3A.240905.015.A2/OS2.0.204.0.VMXINXM:user/release-keys
