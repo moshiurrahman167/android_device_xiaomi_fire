e4a2ffe93accbcfb0fa1a06e89214c14256b6635
moshiurrahman167 <141856473+moshiurrahman167@users.noreply.github.com>
2026-07-24 16:30:25 +0600

    Update twrp_fire.mk

--- twrp_fire.mk
+++ twrp_fire.mk
@@ -9,16 +9,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit TWRP common configuration
+$(call inherit-product, bootable/recovery/openrecoveryscript.mk)
 
 # Inherit from fire device
 $(call inherit-product, device/xiaomi/fire/device.mk)
 
 PRODUCT_DEVICE := fire
-PRODUCT_NAME := omni_fire
+PRODUCT_NAME := twrp_fire
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := fire
+PRODUCT_MODEL := Redmi 12 4G
 PRODUCT_MANUFACTURER := xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
